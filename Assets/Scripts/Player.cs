29fe4606a48bb85d84e833a91c4c2ca976e15b70
Monky518 <zoeyrizor@gmail.com>
2026-06-16 12:25:49 -0700

    Trying New Input System

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,126 +1,25 @@
 using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    public int rotateSpeed;
-    
+    public int speed;
+    public bool canMove = false;
+    public InputActionReference rotateRef;
+    public InputActionReference heightRef;
+
     void Start()
     {
         
     }
 
-    void Update()
+    void FixedUpdate() // update is based on frame rate, fixed update is aligned with physics engine
     {
-        CheckInput();
-    }
-
-    void CheckInput()
-    {
-        //if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
-        //{
-        //    transform.position -= (transform.forward * rotateSpeed * Time.deltaTime);
-        //}
-        //else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-        //{
-        //    transform.position += (transform.forward * rotateSpeed * Time.deltaTime);
-        //}
-
-        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+        if (canMove)
         {
-            transform.Rotate(Vector3.right * rotateSpeed * Time.deltaTime);
+            transform.Rotate(Vector3.up * speed * Time.deltaTime * rotateRef.action.ReadValue<int>());
+            transform.Find("Club").Rotate(Vector3.forward * speed * Time.deltaTime * heightRef.action.ReadValue<int>());
         }
-        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(Vector3.left * rotateSpeed * Time.deltaTime);
-        }
-
-        //else if (Input.GetKeyDown(KeyCode.UpArrow))
-        //{
-        //    ChangePosition(Vector2.up);
-        //}
-
-        //else if (Input.GetKeyDown(KeyCode.DownArrow))
-        //{
-        //    ChangePosition(Vector2.down);
-        //}
     }
-
-//    void ChangePosition(Vector2 d)
-//    {
-//        //checks if you did not click the same direction again
-//        if (d != direction)
-//        {
-//            nextDirection = d;
-//        }
-
-//        if (currentNode != null)
-//        {
-//            Node moveToNode = CanMove(d);
-
-//            if (moveToNode != null)
-//            {
-//                //actively moving
-//                direction = d;
-//                targetNode = moveToNode;
-//                previousNode = currentNode;
-//                currentNode = null;
-//            }
-//        }
-//    }
-
-//    void Move()
-//    {
-//        if (targetNode != currentNode && targetNode != null)
-//        {
-//            //turning around between nodes
-//            if (nextDirection == direction * -1)
-//            {
-//                direction *= -1;
-
-//                Node tempNode = targetNode;
-//                targetNode = previousNode;
-//                previousNode = tempNode;
-//            }
-
-//            if (OverShotTarget())
-//            {
-//                //sets the game's boundaries
-//                currentNode = targetNode;
-//                transform.localPosition = currentNode.transform.position;
-
-//                GameObject otherPortal = GetPortal(currentNode.transform.position);
-//                if (otherPortal != null)
-//                {
-//                    transform.localPosition = otherPortal.transform.position;
-//                    currentNode = otherPortal.GetComponent<Node>();
-//                }
-
-//                Node moveToNode = CanMove(nextDirection);
-//                if (moveToNode != null)
-//                {
-//                    direction = nextDirection;
-//                }
-//                if (moveToNode == null)
-//                {
-//                    moveToNode = CanMove(direction);
-//                }
-//                if (moveToNode != null)
-//                {
-//                    targetNode = moveToNode;
-//                    previousNode = currentNode;
-//                    currentNode = null;
-//                }
-//                else
-//                {
-//                    //hits wall
-//                    direction = Vector2.zero;
-//                }
-//            }
-//            else
-//            {
-//                transform.position += (Vector3)(direction * speed) * Time.deltaTime;
-//            }
-//        }
-//    }
 }
