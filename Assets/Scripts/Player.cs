486705724cb536922c2a62c3e4195cf6e5ef8bf4
Monky518 <zoeyrizor@gmail.com>
2026-07-05 14:16:42 -0700

    IT WORKS
    Got the inputs to work, now working on the rotation limitations, which are harder than they look (i need to work in opposites almost)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,35 +4,51 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    public enum GameMode
+    {
+        MainMenu,
+        PlayerMovement,
+        BallMovement,
+        Results
+    }
+    public GameMode currentGameMode= GameMode.PlayerMovement;
+    
     public float speed;
-    public bool canMove = false;
+    public float heightMin;
+    public float heightMax;
+
     public InputActionReference rotateRef;
     public InputActionReference heightRef;
 
     private float rotate;
     private float height;
+    private GameObject club;
 
     void Start()
     {
-        
+        club = transform.Find("Club").gameObject;
     }
 
     private void Update() // based on frame rate
     {
         rotate = rotateRef.action.ReadValue<float>();
         height = heightRef.action.ReadValue<float>();
-        Debug.Log("Rotate: " +  rotate);
-        Debug.Log("Height: " +  height);
     }
 
     void FixedUpdate() // aligned with physics engine
     {
-        Debug.Log("Fixed Update Works");
-        if (canMove)
+        if (currentGameMode == GameMode.PlayerMovement)
         {
-            Debug.Log("Player Can Move");
             transform.Rotate(Vector3.up * speed * Time.deltaTime * rotate);
-            transform.Find("Club").Rotate(Vector3.forward * speed * Time.deltaTime * height);
+
+            Debug.Log("Club Rotation: " + club.transform.eulerAngles);
+
+            if (club.transform.eulerAngles.z >= heightMin && club.transform.eulerAngles.z <= heightMax)
+                club.transform.Rotate(Vector3.forward * speed * Time.deltaTime * height);
+            else if (club.transform.eulerAngles.z < heightMin)
+                club.transform.eulerAngles = new Vector3(club.transform.eulerAngles.x, club.transform.eulerAngles.y, heightMin);
+            else if (club.transform.eulerAngles.z > heightMax)
+                club.transform.eulerAngles = new Vector3(club.transform.eulerAngles.x, club.transform.eulerAngles.y, heightMax);
         }
     }
 }
