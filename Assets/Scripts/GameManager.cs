12ccc63011c81bfc8741813eecc712d340773436
Monky518 <zoeyrizor@gmail.com>
2026-07-15 13:35:38 -0700

    Script organization and animations
    Organized the scripts better and the animations for the club work
    
    Next, is it time for ball movement

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,114 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
 
-public class GameManager : MonoBehaviour
-{
-    void Start()
-    {
+public class GameManager : MonoBehaviour {
+    public enum GameMode {
+        MainMenu,
+        PlayerPhase,
+        BallMovement,
+        Results
+    }
+    public GameMode currentGameMode = GameMode.PlayerPhase; // temp until menu exists
 
+    public GameObject player;
+    public GameObject powerGauge;
+    public GameObject arrow;
+
+    public InputActionReference rotateRef;
+    public InputActionReference gaugeRef;
+    public InputActionReference doneRef;
+
+    private float powerPercent = 0f;
+
+    private float gaugeHeight = 40f;
+    private float gaugeMinWidth = 5f;
+    private float gaugeMaxWidth = 280f;
+    private float gaugeSpeed = 50f;
+
+    private float arrowStartPosition = -0.15f;
+    private float arrowRange = 0.3f;
+
+    void Update() {
+        switch ( currentGameMode ) {
+            case GameMode.MainMenu:
+                break;
+            case GameMode.PlayerPhase:
+                PlayerPhase();
+                break;
+            case GameMode.BallMovement:
+                BallMovement();
+                break;
+            case GameMode.Results:
+                break;
+        }
     }
 
-    void Update()
-    {
+    void PlayerPhase() {
+        // check if player is done
+        if ( doneRef.action.ReadValue<float>() == 1f )
+            currentGameMode = GameMode.BallMovement;
+        else {
+            // check rotate movement
+            if ( rotateRef.action.ReadValue<float>() != 0f )
+                player.GetComponent<Player>().RotateClub( rotateRef.action.ReadValue<float>() );
 
+            // check gauge movement
+            if ( gaugeRef.action.ReadValue<float>() != 0f ) {
+                // validate power level
+                if ( powerPercent <= 1f && powerPercent >= 0f )
+                    powerPercent += gaugeSpeed * (gaugeRef.action.ReadValue<float>() / 100) * Time.deltaTime;
+                else if ( powerPercent > 1f )
+                    powerPercent = 1f;
+                else if ( powerPercent < 0f )
+                    powerPercent = 0f;
+
+                // update UI
+                powerGauge.transform.Find( "Gauge" ).GetComponent<RectTransform>().sizeDelta = new Vector2( gaugeMaxWidth * powerPercent + gaugeMinWidth,gaugeHeight );
+                arrow.GetComponent<RectTransform>().localPosition = new Vector3( 0,arrowStartPosition + (arrowRange * powerPercent),0 );
+            }
+        }
+    }
+
+    void BallMovement() {
+        // get rid of player ui
+        powerGauge.SetActive( false );
+        arrow.SetActive( false );
+
+        // start animation
+        switch ( powerPercent ) {
+            case < 0.18f:
+                player.transform.Find( "Club" ).GetComponent<Animation>().Play( "ClubSwing10" );
+                break;
+            case >= 0.18f and < 0.34f:
+                player.transform.Find( "Club" ).GetComponent<Animation>().Play( "ClubSwing20" );
+                break;
+            case >= 0.34f and < 0.51f:
+                player.transform.Find( "Club" ).GetComponent<Animation>().Play( "ClubSwing30" );
+                break;
+            case >= 0.51f and < 0.68f:
+                player.transform.Find( "Club" ).GetComponent<Animation>().Play( "ClubSwing40" );
+                break;
+            case >= 0.68f and < 0.84f:
+                player.transform.Find( "Club" ).GetComponent<Animation>().Play( "ClubSwing50" );
+                break;
+            case >= 0.84f:
+                player.transform.Find( "Club" ).GetComponent<Animation>().Play( "ClubSwing60" );
+                break;
+        }
+
+        // wait for animation
+        StartCoroutine( WaitForAnimation() );
+
+        // ball moves and camera follows
+        // reset player ui
+        // if not in hole, start player phase
+        // else, start results phase
+    }
+
+    IEnumerator WaitForAnimation() {
+        yield return new WaitForSeconds( 1f );
+        Debug.Log( "Animation Complete!" );
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,72 +1,9 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
-{
-    public enum GameMode
-    {
-        MainMenu,
-        PlayerMovement,
-        BallMovement,
-        Results
-    }
-    public GameMode currentGameMode = GameMode.PlayerMovement;
-    
+public class Player : MonoBehaviour {
     public float rotateSpeed;
-    public float gaugeSpeed;
 
-    public InputActionReference rotateRef;
-    public InputActionReference gaugeRef;
-    public InputActionReference endPlayerMovementRef;
-
-    public RectTransform gauge;
-
-    private GameObject club;
-    private float powerPercent = 0f;
-
-    private float gaugeHeight = 40f;
-    private float gaugeMinWidth = 5f;
-    private float gaugeMaxWidth = 280f;
-
-    void Start()
-    {
-        club = transform.Find("Club").gameObject;
-    }
-
-    private void Update() // based on frame rate
-    {
-        if (currentGameMode == GameMode.PlayerMovement)
-        {
-            if (!endPlayerMovementRef.action.ReadValue<Boolean>())
-            {
-                transform.Rotate(Vector3.up * rotateSpeed * rotateRef.action.ReadValue<float>() * Time.deltaTime);
-
-                if (powerPercent <= 1f && powerPercent >= 0f)
-                    powerPercent += gaugeSpeed * (gaugeRef.action.ReadValue<float>() / 100) * Time.deltaTime;
-                else if (powerPercent > 1f)
-                    powerPercent = 1f;
-                else if (powerPercent < 0f)
-                    powerPercent = 0f;
-
-                   gauge.sizeDelta = new Vector2(gaugeMaxWidth * powerPercent + gaugeMinWidth, gaugeHeight);
-            } else
-            {
-                currentGameMode = GameMode.BallMovement;
-            }
-        } else if (currentGameMode == GameMode.BallMovement)
-        {
-            // get rid of player ui
-            // start animation
-            // ball moves and camera follows
-            // reset player ui
-            // if not in hole, start player phase
-            // else, start results phase
-        }
-    }
-
-    void FixedUpdate() // aligned with physics engine
-    {
-
+    public void RotateClub( float direction ) {
+        transform.Rotate( Vector3.up * rotateSpeed * direction * Time.deltaTime );
     }
 }
